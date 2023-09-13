77b0c44a30139273608c15103e57cf6e09de058a
Julia <julia.gurbanova@gmail.com>
2023-09-13 18:51:19 +0300

    add: a form so that the user can view and edit issues let the user select multiple tags

---
+++ Portfolio/NoIssueView.swift
@@ -0,0 +1,30 @@
+//
+//  NoIssueView.swift
+//  Portfolio
+//
+//  Created by Julia Gurbanova on 12.09.2023.
+//
+
+import SwiftUI
+
+struct NoIssueView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        VStack {
+            Text("No Issue Selected")
+                .font(.title)
+                .foregroundStyle(.secondary)
+            
+            Button("New Issue") {
+                // make a new issue
+            }
+        }
+    }
+}
+
+struct NoIssueView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoIssueView()
+    }
+}
