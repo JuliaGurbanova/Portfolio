3e3bac9ecb4461416907e3384e40abd38817c01a
Julia <julia.gurbanova@gmail.com>
2023-09-27 16:35:57 +0300

    chore: clean up views

---
+++ Portfolio/SmartFilterRow.swift
@@ -0,0 +1,22 @@
+//
+//  SmartFilterRow.swift
+//  Portfolio
+//
+//  Created by Julia Gurbanova on 27.09.2023.
+//
+
+import SwiftUI
+
+struct SmartFilterRow: View {
+    var filter: Filter
+    
+    var body: some View {
+        NavigationLink(value: filter) {
+            Label(LocalizedStringKey(filter.name), systemImage: filter.icon)
+        }
+    }
+}
+
+#Preview {
+    SmartFilterRow(filter: .all)
+}
